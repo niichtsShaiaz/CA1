8c7f80599688af4e3e60f1098dba1ad63ee98a78
Unknown <e.z.l@live.com>
2018-02-15 15:45:34 +0100

    skeleton complete

---
+++ js/include.js
@@ -0,0 +1,8 @@
+document.getElementById("menu").innerHTML =
+    "<ul>\n" +
+    "    <li><a href='index.html'>Homepage</a></li>\n" +
+    "    <li><a href='Calculator.html'>Calculator</a></li>\n" +
+    "    <li><a href='SpeedTester.html'>Typing Speed Tester</a></li>\n" +
+    "    <li><a href='DomManipulation.html'>Dynamic UI manipulation</a></li>\n" +
+    "    <li><a href='DynamicUi.html'>Dynamic UI manipulation with data obtained via fetch</a></li>\n" +
+    "</ul>";
